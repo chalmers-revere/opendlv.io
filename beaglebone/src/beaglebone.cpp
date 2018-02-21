c49b15bbf195889552530e0d9fda2556b3ca9257
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2018-02-21 13:04:53 +0100

    New module

---
+++ beaglebone/src/beaglebone.cpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2018  Christian Berger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cluon-complete.hpp"
+#include "opendlv-standard-message-set.hpp"
+
+#include "proxy-beaglebone.hpp"
+
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <thread>
+
+int32_t main(int32_t argc, char **argv) {
+    int32_t retCode{0};
+    auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
+    if ( (0 == commandlineArguments.count("oxts_port")) || (0 == commandlineArguments.count("cid")) ) {
+        std::cerr << argv[0] << " decodes latitude/longitude/heading from an OXTS GPS/INSS unit and publishes it to a running OpenDaVINCI session using the OpenDLV Standard Message Set." << std::endl;
+        std::cerr << "Usage:   " << argv[0] << " [--oxts_ip=<IPv4-address>] --oxts_port=<port> --cid=<OpenDaVINCI session> [--id=<Identifier in case of multiple OxTS units>] [--verbose]" << std::endl;
+        std::cerr << "Example: " << argv[0] << " --oxts_ip=0.0.0.0 --oxts_port=3000 --cid=111" << std::endl;
+        retCode = 1;
+    } else {
+        const uint32_t ID{(commandlineArguments["id"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
+        const bool VERBOSE{commandlineArguments.count("verbose") != 0};
+
+        // Interface to a running OpenDaVINCI session (ignoring any incoming Envelopes).
+        cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"])),
+            [](auto){}
+        };
+
+        // Interface to OxTS.
+        const std::string OXTS_ADDRESS((commandlineArguments.count("oxts_ip") == 0) ? "0.0.0.0" : commandlineArguments["oxts_ip"]);
+        const std::string OXTS_PORT(commandlineArguments["oxts_port"]);
+        OxTSDecoder oxtsDecoder;
+        cluon::UDPReceiver fromOXTS(OXTS_ADDRESS, std::stoi(OXTS_PORT),
+            [&od4Session = od4, &decoder=oxtsDecoder, senderStamp = ID, VERBOSE](std::string &&d, std::string &&/*from*/, std::chrono::system_clock::time_point &&tp) noexcept {
+            auto retVal = decoder.decode(d);
+            if (retVal.first) {
+                cluon::data::TimeStamp sampleTime = cluon::time::convert(tp);
+
+                opendlv::proxy::GeodeticWgs84Reading msg1 = retVal.second.first;
+                od4Session.send(msg1, sampleTime, senderStamp);
+
+                opendlv::proxy::GeodeticHeadingReading msg2 = retVal.second.second;
+                od4Session.send(msg2, sampleTime, senderStamp);
+
+                // Print values on console.
+                if (VERBOSE) {
+                    std::stringstream buffer;
+                    msg1.accept([](uint32_t, const std::string &, const std::string &) {},
+                               [&buffer](uint32_t, std::string &&, std::string &&n, auto v) { buffer << n << " = " << v << '\n'; },
+                               []() {});
+                    std::cout << buffer.str() << std::endl;
+
+                    std::stringstream buffer2;
+                    msg2.accept([](uint32_t, const std::string &, const std::string &) {},
+                               [&buffer2](uint32_t, std::string &&, std::string &&n, auto v) { buffer2 << n << " = " << v << '\n'; },
+                               []() {});
+                    std::cout << buffer2.str() << std::endl;
+                }
+            }
+        });
+
+        // Just sleep as this microservice is data driven.
+        using namespace std::literals::chrono_literals;
+        while (od4.isRunning()) {
+            std::this_thread::sleep_for(1s);
+        }
+    }
+    return retCode;
+}
---
+++ beaglebone/src/proxy-beaglebone.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2018  Christian Berger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cluon-complete.hpp"
+#include "proxy-beaglebone.hpp"
+
+#include <cmath>
+#include <cstring>
+#include <array>
+#include <sstream>
+#include <string>
+
+std::pair<bool, std::pair<opendlv::proxy::GeodeticWgs84Reading, opendlv::proxy::GeodeticHeadingReading> >
+    OxTSDecoder::decode(const std::string &data) noexcept {
+    bool retVal{false};
+    opendlv::proxy::GeodeticWgs84Reading gps;
+    opendlv::proxy::GeodeticHeadingReading heading;
+
+    constexpr std::size_t OXTS_PACKET_LENGTH{72};
+    constexpr uint8_t OXTS_FIRST_BYTE{0xE7};
+    if ( (OXTS_PACKET_LENGTH == data.size()) && (OXTS_FIRST_BYTE == static_cast<uint8_t>(data.at(0))) ) {
+        std::stringstream buffer{data};
+
+        // Decode latitude/longitude.
+        {
+            double latitude{0.0};
+            double longitude{0.0};
+
+            // Move to where latitude/longitude are encoded.
+            constexpr uint32_t START_OF_LAT_LON{23};
+            buffer.seekg(START_OF_LAT_LON);
+            buffer.read(reinterpret_cast<char*>(&latitude), sizeof(double));
+            buffer.read(reinterpret_cast<char*>(&longitude), sizeof(double));
+
+            gps.latitude(latitude / M_PI * 180.0).longitude(longitude / M_PI * 180.0);
+        }
+
+        // Decode heading.
+        {
+            float northHeading{0.0f};
+
+            // Move to where heading is encoded.
+            constexpr uint32_t START_OF_HEADING{52};
+            buffer.seekg(START_OF_HEADING);
+
+            // Extract only three bytes from OxTS.
+            std::array<char, 4> tmp{0, 0, 0, 0};
+            buffer.read(tmp.data(), 3);
+            uint32_t value{0};
+            std::memcpy(&value, tmp.data(), 4);
+            value = le32toh(value);
+            northHeading = value * 1e-6f;
+
+            // Normalize between -M_PI .. M_PI.
+            while (northHeading < -M_PI) {
+                northHeading += 2.0f * static_cast<float>(M_PI);
+            }
+            while (northHeading > M_PI) {
+                northHeading -= 2.0f * static_cast<float>(M_PI);
+            }
+
+            heading.northHeading(northHeading);
+        }
+        retVal = true;
+    }
+    return std::make_pair(retVal, std::make_pair(gps, heading));
+}
+
